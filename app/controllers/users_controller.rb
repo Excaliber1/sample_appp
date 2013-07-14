61e095bd75382b81dfc2ec923662422a8fc583b4
Connor Cayson <CPCAYSON@gmail.com>
2013-07-14 19:40:15 -0400

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,16 @@ class UsersController < ApplicationController
   	 @user = User.new
   end
 
- def create
+def create
     @user = User.new(user_params)
     if @user.save
-       flash[:success] = "Welcome   <%= @user.name %>  to BaBel"
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
-
   private
 
     def user_params
